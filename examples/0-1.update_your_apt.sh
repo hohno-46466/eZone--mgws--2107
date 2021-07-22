668127f9f2f5c5c50eed69c7d3db0f70530aac8a
hohno-46466 <hohno.46466@gmail.com>
2021-07-23 05:51:35 +0900

    updated by @hohno_at_kuimc on hohno-SP6-2019H

---
+++ examples/0-1.update_your_apt.sh
@@ -0,0 +1,9 @@
+#
+# All commands for the workshop
+#
+# Last update: Thu Jul 22 20:59:12 JST 2021 by @hohno_at_kuimc
+#
+# Public domain
+#
+
+# ----------------------------------------------------------
--- examples/README.md
+++ examples/README.md
@@ -1,1 +1,8 @@
 # eZone--mgws--2107/examples
+
+    ./examples/
+     ├── 1-1.MQTT1.sh
+     ├── 1-2.MQTT2.sh
+     └── README.md
+     
+     0 directories, 3 files
