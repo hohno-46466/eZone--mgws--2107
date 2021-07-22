4f9b1813b1ec80dc839bc1b679da8608f8adcdb9
hohno-46466 <hohno.46466@gmail.com>
2021-07-23 05:19:38 +0900

    updated by @hohno_at_kuimc on hohno-SP6-2019H

--- all_commands.sh
+++ all_commands.sh
@@ -7,27 +7,3 @@
 #
 
 # ----------------------------------------------------------
-
-# (1) Basic messaging over MQTT
-
-# (1-1) Starting MQTT Broker
-
-(T1)$ mosquitto
-
-# ----------------------------------------------------------
-
-# (1-2) Subscribe (receive) message from MQTT broker on Terminal No.2 (T2)
-
-(T2)$ mosquitto_sub -t MGWS/Test99
-
-# ----------------------------------------------------------
-
-# (1-3) Publish (send) a message from Terminal No.2 (T2) to Terminal No.3 (T3)
-
-(T3)$ echo "Hello,Halifax!" 
-(T3)$ echo "Hello,Halifax!" | mosquitto_pub -l -t MGWS/Test99
-
-
-# ----------------------------------------------------------
-
-
---
+++ examples/1-1.MQTT1.sh
@@ -0,0 +1,31 @@
+#
+# (1-1) Basic messaging over MQTT
+#
+# Last update: Thu Jul 22 20:59:12 JST 2021 by @hohno_at_kuimc
+#
+# Public domain
+#
+
+# ----------------------------------------------------------
+
+# (Step1) Starting MQTT Broker
+
+(T1)$ mosquitto
+
+# ----------------------------------------------------------
+
+# (Step2) Subscribe (receive) message from MQTT broker on Terminal No.2 (T2)
+
+(T2)$ mosquitto_sub -t MGWS/Test99
+
+# ----------------------------------------------------------
+
+# (Step3) Publish (send) a message from Terminal No.2 (T2) to Terminal No.3 (T3)
+
+(T3)$ echo "Hello,Halifax!" 
+(T3)$ echo "Hello,Halifax!" | mosquitto_pub -l -t MGWS/Test99
+
+
+# ----------------------------------------------------------
+
+
