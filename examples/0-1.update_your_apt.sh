6ee48b51e7cd1423b982eadcff740e4a09f771cb
hohno-46466 <hohno.46466@gmail.com>
2021-07-23 11:49:35 +0900

    updated by @hohno_at_kuimc on hohno-SP6-2019H

--- examples/0-1.update_your_apt.sh
+++ examples/0-1.update_your_apt.sh
@@ -1,5 +1,5 @@
 #
-# All commands for the workshop
+# Update and upgrade your apt environment 
 #
 # Last update: Thu Jul 22 20:59:12 JST 2021 by @hohno_at_kuimc
 #
@@ -7,3 +7,19 @@
 #
 
 # ----------------------------------------------------------
+
+# (Step1) Update your apt environment (update the database)
+
+$ sudo apt update
+
+# ----------------------------------------------------------
+
+# (Step2) Upgrade your apt environment (update the installed packages)
+
+$ sudo apt upgrade
+
+# ----------------------------------------------------------
+
+# (Step3) Install important commands
+
+$ sudo apt install cu gawk nkf
