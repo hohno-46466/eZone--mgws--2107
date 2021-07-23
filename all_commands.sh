81335831fe4f21b89169baac46e811fd8762cdc1
hohno-46466 <hohno.46466@gmail.com>
2021-07-23 11:50:03 +0900

    updated by @hohno_at_kuimc on hohno-SP6-2019H

--- all_commands.sh
+++
@@ -1,9 +0,0 @@
-#
-# All commands for the workshop
-#
-# Last update: Thu Jul 22 20:59:12 JST 2021 by @hohno_at_kuimc
-#
-# Public domain
-#
-
-# ----------------------------------------------------------
